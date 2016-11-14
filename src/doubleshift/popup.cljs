e1222960be52de994776e40fe876c183bd27e882
Prakhar Srivastav <prakhar1989@gmail.com>
2016-11-14 16:40:08 -0500

    setting a random tab to be active

--- src/doubleshift/popup.cljs
+++ src/doubleshift/popup.cljs
@@ -5,12 +5,17 @@
             [cljs.core.async :refer [>! <!]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
+(defn set-active [tab]
+  (let [{id :id} tab]
+    (tabs/update id {:highlighted true})))
+
 (defn log-tabs []
   (let [tabs-chan (tabs/query {:currentWindow true})]
     (go (when-let [alltabs (<! tabs-chan)]
           (->> alltabs
                (map #(select-keys % [:url :title :id]))
-               (console/log))))))
+               (rand-nth)
+               (set-active))))))
 
 (defn init []
   (log-tabs)
