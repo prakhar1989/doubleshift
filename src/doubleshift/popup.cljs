778b504be7426df5e77259c83d4989261a9ff9d8
Prakhar Srivastav <prakhar1989@gmail.com>
2016-11-14 16:19:16 -0500

    version bump on dep. got tab list running

--- src/doubleshift/popup.cljs
+++ src/doubleshift/popup.cljs
@@ -1,10 +1,17 @@
 (ns doubleshift.popup
   (:require [khroma.runtime :as runtime]
             [khroma.log :as console]
+            [khroma.tabs :as tabs]
             [cljs.core.async :refer [>! <!]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
+(defn log-tabs []
+  (let [tabs-chan (tabs/query {:currentWindow true})]
+    (go (when-let [alltabs (<! tabs-chan)]
+          (->> alltabs
+               (map #(select-keys % [:url :title :id]))
+               (console/log))))))
+
 (defn init []
-  (let [bg (runtime/connect)]
-    (go (>! bg :lol-i-am-a-popup)
-        (console/log "Background said: " (<! bg)))))
+  (log-tabs)
+  (console/log "hello world from doubleshift"))
